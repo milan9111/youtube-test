fd526aea279bdede9c0534ee81499b9d6f2fd09c
VasylZV <milan9111work@ukr.net>
2023-10-23 22:51:23 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -21,6 +21,24 @@ function App() {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
       ></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/yMzyleT2FqY?si=TjY_ORSIEB7WwNv_"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/4Hihge6FFVw?si=XEo18991cHsxfpmQ"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 }
